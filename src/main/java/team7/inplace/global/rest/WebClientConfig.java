be0f1aca59188e86d928ac4ca06954c6c9548378
suhyeon7497 <nucc1186@gmail.com>
2024-10-31 20:23:51 +0900

    [fix] WebClientConfig에 @Configuration 추가
    관련 이슈: #50

--- src/main/java/team7/inplace/global/rest/WebClientConfig.java
+++ src/main/java/team7/inplace/global/rest/WebClientConfig.java
@@ -1,8 +1,10 @@
 package team7.inplace.global.rest;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfig {
 
     @Bean
