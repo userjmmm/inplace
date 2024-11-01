4c70031b66a002be5f4dd664c325ded8cc11c8bc
sanghee0820 <lsh277604@gmail.com>
2024-11-01 21:42:42 +0900

    [feat] 비디오 조회수 업데이트 command추가

--- src/main/java/team7/inplace/crawling/application/dto/CrawlingVideoViewInfo.java
+++ src/main/java/team7/inplace/crawling/application/dto/CrawlingVideoViewInfo.java
@@ -1,6 +1,7 @@
 package team7.inplace.crawling.application.dto;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import team7.inplace.video.application.command.VideoCommand;
 
 public record CrawlingVideoViewInfo(
         Long videoId,
@@ -8,5 +9,9 @@ public record CrawlingVideoViewInfo(
 ) {
     public static CrawlingVideoViewInfo of(Long videoId, JsonNode videoDetail) {
         return new CrawlingVideoViewInfo(videoId, videoDetail);
+    }
+
+    public VideoCommand.UpdateViewCount toVideoCommand() {
+        return VideoCommand.UpdateViewCount.from(videoDetail().path("items").get(0).get("statistics"), videoId());
     }
 }
