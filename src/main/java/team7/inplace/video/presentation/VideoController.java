59728c962aaef7d11e592c6aeeeb2212080cd2d0
sanghee0820 <lsh277604@gmail.com>
2024-11-08 21:48:28 +0900

    [fix] import 수정

--- src/main/java/team7/inplace/video/presentation/VideoController.java
+++ src/main/java/team7/inplace/video/presentation/VideoController.java
@@ -1,7 +1,5 @@
 package team7.inplace.video.presentation;
 
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,9 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team7.inplace.video.application.VideoFacade;
 import team7.inplace.video.application.VideoService;
