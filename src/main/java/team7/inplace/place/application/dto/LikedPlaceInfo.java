22ec428870a762bb905dc616d04e81e2a8d6884c
hyunseo <wndlthsk@naver.com>
2024-11-11 17:46:24 +0900

    [feat] 마이페이지 좋아요한 장소 반환

---
+++ src/main/java/team7/inplace/place/application/dto/LikedPlaceInfo.java
@@ -0,0 +1,23 @@
+package team7.inplace.place.application.dto;
+
+
+import team7.inplace.likedPlace.domain.LikedPlace;
+
+public record LikedPlaceInfo(
+    Long placeId,
+    String placeName,
+    String imageUrl,
+    String influencerName,
+    boolean likes
+) {
+
+    public static LikedPlaceInfo of(LikedPlace likedPlace, String influencerName) {
+        return new LikedPlaceInfo(
+            likedPlace.getPlace().getId(),
+            likedPlace.getPlace().getName(),
+            likedPlace.getPlace().getMenuImgUrl(),
+            influencerName,
+            likedPlace.isLiked()
+        );
+    }
+}
