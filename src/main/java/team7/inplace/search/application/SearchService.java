95f876ba02cdae3fd79260a6b7c057405955e721
sanghee0820 <lsh277604@gmail.com>
2024-11-13 21:49:14 +0900

    [feat] Keyword로 Video 검색 기능 추가

--- src/main/java/team7/inplace/search/application/SearchService.java
+++ src/main/java/team7/inplace/search/application/SearchService.java
@@ -6,10 +6,13 @@ import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team7.inplace.search.application.dto.AutoCompletionInfo;
 import team7.inplace.search.application.dto.SearchType;
 import team7.inplace.search.persistence.InfluencerSearchRepository;
 import team7.inplace.search.persistence.PlaceSearchRepository;
+import team7.inplace.search.persistence.VideoSearchRepository;
+import team7.inplace.video.application.dto.VideoInfo;
 
 @Service
 @Slf4j
@@ -17,12 +20,13 @@ import team7.inplace.search.persistence.PlaceSearchRepository;
 public class SearchService {
     private static final Integer MAX_COMPLETION_RESULTS = 5;
 
+    private final VideoSearchRepository videoSearchRepository;
     private final InfluencerSearchRepository influencerSearchRepository;
     private final PlaceSearchRepository placeSearchRepository;
 
     public List<AutoCompletionInfo> searchAutoCompletions(String keyword) {
-        var influencerSearchInfo = influencerSearchRepository.searchSimilarKeywords(keyword);
-        var placeSearchInfo = placeSearchRepository.searchSimilarKeywords(keyword);
+        var influencerSearchInfo = influencerSearchRepository.searchEntityByKeywords(keyword);
+        var placeSearchInfo = placeSearchRepository.searchEntityByKeywords(keyword);
 
         var influencerAutoComplete = influencerSearchInfo.stream()
                 .map(info -> new AutoCompletionInfo(info.searchResult().getName(), info.score(), SearchType.INFLUENCER))
@@ -35,6 +39,17 @@ public class SearchService {
         return Stream.concat(influencerAutoComplete.stream(), placeAutoComplete.stream())
                 .sorted(Comparator.comparing(AutoCompletionInfo::score).reversed())
                 .limit(MAX_COMPLETION_RESULTS)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<VideoInfo> searchVideo(String keyword) {
+        var videoInfos = videoSearchRepository.searchEntityByKeywords(keyword);
+        
+        return videoInfos.stream()
+                .map(videoInfo -> {
+                    return VideoInfo.from(videoInfo.searchResult());
+                })
                 .toList();
     }
 }
