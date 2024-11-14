6c3f977d4456a743ddfe88079740d6172aa32fb2
sanghee0820 <lsh277604@gmail.com>
2024-11-15 06:15:45 +0900

    [fix] api path 수정

--- src/main/java/team7/inplace/user/presentation/TempController.java
+++ src/main/java/team7/inplace/user/presentation/TempController.java
@@ -2,11 +2,13 @@ package team7.inplace.user.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team7.inplace.security.util.AuthorizationUtil;
 import team7.inplace.user.persistence.UserRepository;
 
-@RestController("/temp")
+@RestController
+@RequestMapping("/temp")
 @RequiredArgsConstructor
 public class TempController {
     private final UserRepository userRepository;
