fd69fdfda6e871e471eedbe11a123c76243de60d
suhyeon7497 <nucc1186@gmail.com>
2024-11-15 15:16:44 +0900

    [fix] authentication 실패 시 401 반환

--- src/main/java/team7/inplace/security/entryPoint/LoginAuthenticationEntryPoint.java
+++ src/main/java/team7/inplace/security/entryPoint/LoginAuthenticationEntryPoint.java
@@ -1,24 +1,38 @@
 package team7.inplace.security.entryPoint;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import team7.inplace.global.exception.InplaceException;
+import team7.inplace.global.exception.code.AuthorizationErrorCode;
 
 public class LoginAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    @Value("${spring.redirect.front-end-url}")
-    private String frontEndUrl;
+    private final ObjectMapper objectMapper;
+
+    public LoginAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException {
-        if (request.getHeader("origin").equals(frontEndUrl)) {
-            response.sendRedirect("/");
-            return;
-        }
-        response.sendRedirect(frontEndUrl);
+        setErrorResponse(response, InplaceException.of(AuthorizationErrorCode.NOT_AUTHENTICATION));
+    }
+
+    private void setErrorResponse(
+        HttpServletResponse response,
+        InplaceException inplaceException
+    ) throws IOException {
+        response.setStatus(inplaceException.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+            inplaceException.getHttpStatus(), inplaceException.getMessage());
+        response.getWriter().write(objectMapper.writeValueAsString(problemDetail));
     }
 }
