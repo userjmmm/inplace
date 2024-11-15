2eedeae5233fc635b350bd1dd00b4216b6203323
suhyeon7497 <nucc1186@gmail.com>
2024-11-15 16:22:46 +0900

    [test] Authentication 실패시 Test 변경

--- src/test/java/team7/inplace/security/config/SecurityConfigTest.java
+++ src/test/java/team7/inplace/security/config/SecurityConfigTest.java
@@ -1,5 +1,11 @@
 package team7.inplace.security.config;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,11 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,29 +25,29 @@ class SecurityConfigTest {
     @WithMockUser(roles = "USER")
     void testAdminEndpoints() throws Exception {
         mockMvc.perform(get("/admin/video"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(header().string("Location", containsString("inplace.my")));
         mockMvc.perform(get("/admin/error-logs"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(header().string("Location", containsString("inplace.my")));
         mockMvc.perform(post("/api/error-logs/1/video"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(header().string("Location", containsString("inplace.my")));
         mockMvc.perform(get("/cicd"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(header().string("Location", containsString("inplace.my")));
         mockMvc.perform(post("/crawling/video/1/place/1"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(header().string("Location", containsString("inplace.my")));
     }
 
     @Test
     @WithAnonymousUser
     void testAnonymousEndpoints() throws Exception {
         mockMvc.perform(get("/admin/video"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(status().isUnauthorized());
         mockMvc.perform(get("/admin/error-logs"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(status().isUnauthorized());
         mockMvc.perform(post("/api/error-logs/1/video"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(status().isUnauthorized());
         mockMvc.perform(get("/cicd"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(status().isUnauthorized());
         mockMvc.perform(post("/influencers/likes"))
-                .andExpect(header().string("Location", containsString("inplace.my")));
+            .andExpect(status().isUnauthorized());
     }
 }
