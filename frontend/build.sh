a6e39e41f70a0885d89059c95203bf158b071b80
userjmmm <141299582+userjmmm@users.noreply.github.com>
2024-12-31 14:41:34 +0900

    [INPLACE-48] 프론트엔드 dev 환경에서 발생하는 오류 수정 (#55)
    * [INPLACE-19] secrets으로 수정 (#29)
    
    * INPLACE-19 백엔드 인스턴스 분리
    
    * INPLACE-19 aws 자격 증명, 태그 업데이트 단계 추가
    
    * INPLACE-19 ssh 순서 변경
    
    * INPLACE-19 secrets으로 수정
    
    * [INPLACE-19] secrets으로 수정2 (#30)
    
    * INPLACE-19 백엔드 인스턴스 분리
    
    * INPLACE-19 aws 자격 증명, 태그 업데이트 단계 추가
    
    * INPLACE-19 ssh 순서 변경
    
    * INPLACE-19 secrets으로 수정
    
    * INPLACE-19 secrets으로 수정2
    
    * [INPLACE-19] 로그 출력 추가 (#31)
    
    * INPLACE-19 백엔드 인스턴스 분리
    
    * INPLACE-19 aws 자격 증명, 태그 업데이트 단계 추가
    
    * INPLACE-19 ssh 순서 변경
    
    * INPLACE-19 secrets으로 수정
    
    * INPLACE-19 secrets으로 수정2
    
    * INPLACE-19 로그 출력 추가
    
    * INPLACE-19 변수 수정 (#32)
    
    * INPLACE-19 조건문 출력 추가 (#33)
    
    * INPLACE-19 타겟 인스턴스 변수 수정 (#34)
    
    * INPLACE-19 컨테이너 삭제 서버 변경 (#36)
    
    * INPLACE-19 모든 규칙의 타겟그룹이 변경되도록 수정 (#37)
    
    * INPLACE-19 리스너 기본 규칙 분리 (#38)
    
    * INPLACE-19 큰따옴표 추가 (#39)
    
    * INPLACE-19 규칙 가져오기 구조 변경 (#40)
    
    * INPLACE-19 규칙 가져오기 배열로 변경 (#41)
    
    * INPLACE-19 시크릿 이름 변경 (#42)
    
    * INPLACE-19 헬스체크 포트 분리 (#43)
    
    * [INPLACE-48] 모든 경로를 index.html로 리다이렉트하도록 새로고침 오류 수정
    
    * [INPLACE-48] sync-fork CI/CD에서 발생하는 output 디렉토리 생성 오류 해결을 위한 경로 수정
    
    * [INPLACE-48] 현재 사용하지 않는 백엔드 ci-cd 제거
    
    ---------
    
    Co-authored-by: wndlthsk <80496766+wndlthsk@users.noreply.github.com>

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-cd ..
-mkdir output
-cp -R ./inplace/frontend/* ./output
-cp -R ./output ./inplace/frontend/
+
+# output 디렉토리 생성
+mkdir -p ../output
+
+# frontend 디렉토리의 내용을 output으로 복사
+cp -R ./* ../output/
+
+# 디버깅을 위한 디렉토리 구조 출력
+ls -la ../output/
