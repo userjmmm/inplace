49509da62d0d778762cddfdb3e2d566578742da4
userjmmm <141299582+userjmmm@users.noreply.github.com>
2025-01-01 20:30:08 +0900

    [INPLACE-48] vercel 배포 오류 수정 및 디버깅용 코드 추가 (#58)
    * [INPLACE-19] secrets으로 수정 (#29)
    
    * INPLACE-19 백엔드 인스턴스 분리
    
    * INPLACE-19 aws 자격 증명, 태그 업데이트 단계 추가
    
    * INPLACE-19 ssh 순서 변경
    
    * INPLACE-19 secrets으로 수정
    
    * [INPLACE-19] secrets으로 수정2 (#30)
    
    * INPLACE-19 백엔드 인스턴스 분리
    
    * INPLACE-19 aws 자격 증명, 태그 업데이트 단계 추가
    
    * INPLACE-19 ssh 순서 변경
    
    * INPLACE-19 secrets으로 수정
    
    * INPLACE-19 secrets으로 수정2
    
    * [INPLACE-19] 로그 출력 추가 (#31)
    
    * INPLACE-19 백엔드 인스턴스 분리
    
    * INPLACE-19 aws 자격 증명, 태그 업데이트 단계 추가
    
    * INPLACE-19 ssh 순서 변경
    
    * INPLACE-19 secrets으로 수정
    
    * INPLACE-19 secrets으로 수정2
    
    * INPLACE-19 로그 출력 추가
    
    * INPLACE-19 변수 수정 (#32)
    
    * INPLACE-19 조건문 출력 추가 (#33)
    
    * INPLACE-19 타겟 인스턴스 변수 수정 (#34)
    
    * INPLACE-19 컨테이너 삭제 서버 변경 (#36)
    
    * INPLACE-19 모든 규칙의 타겟그룹이 변경되도록 수정 (#37)
    
    * INPLACE-19 리스너 기본 규칙 분리 (#38)
    
    * INPLACE-19 큰따옴표 추가 (#39)
    
    * INPLACE-19 규칙 가져오기 구조 변경 (#40)
    
    * INPLACE-19 규칙 가져오기 배열로 변경 (#41)
    
    * INPLACE-19 시크릿 이름 변경 (#42)
    
    * INPLACE-19 헬스체크 포트 분리 (#43)
    
    * [INPLACE-48] 모든 경로를 index.html로 리다이렉트하도록 새로고침 오류 수정
    
    * [INPLACE-48] sync-fork CI/CD에서 발생하는 output 디렉토리 생성 오류 해결을 위한 경로 수정
    
    * [INPLACE-48] 현재 사용하지 않는 백엔드 ci-cd 제거
    
    * [INPLACE-48] vercel build 오류를 해결하기 위해 build.sh에서 모든 디렉토리를 복사하도록 수정
    
    * [INPLACE-48] ci/cd 내 경로 수정 및 디버깅용 코드 추가
    
    ---------
    
    Co-authored-by: wndlthsk <80496766+wndlthsk@users.noreply.github.com>

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# 전체 프로젝트를 복사할 output 디렉토리 생성
+# 기존 output 디렉토리가 있다면 삭제
+rm -rf ../output
+
+# output 디렉토리 생성 (frontend의 상위 디렉토리에)
 mkdir -p ../output
 
-# 현재 프로젝트의 모든 디렉토리를 output으로 복사
-cp -R ../../* ../output/
+# 프로젝트 구조를 output으로 복사
+cp -R ../.github ../.hooks ../backend ../frontend ../.gitignore ../output/
 
 # frontend 디렉토리의 최신 내용으로 업데이트
-rm -rf ../output/frontend/*  # frontend 디렉토리 내용을 비우고
-cp -R ./* ../output/frontend/  # 현재 frontend 내용을 복사
+cp -R * ../output/frontend/
 
 # 디버깅을 위한 디렉토리 구조 출력
+echo "Output directory structure:"
 ls -la ../output/
