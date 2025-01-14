a4ca339788fd6fe7b02b55b72dd98b2ea5c90308
sanghee0820 <lsh277604@gmail.com>
2025-01-14 12:12:15 +0900

    [INPLACE-87] 리팩토링 시 발생한 에러와 추가 리팩토링 진행 (#68)
    * INPLACE-87 브랜치 이름 검사 시 대문자도 포함하도록 수정
    
    * INPLACE-87 EnableJPARepository 삭제
    
    * INPLACE-87 프로필을 하나로 병합

--- backend/src/main/java/team7/inplace/InplaceApplication.java
+++ backend/src/main/java/team7/inplace/InplaceApplication.java
@@ -3,22 +3,10 @@ package team7.inplace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import team7.inplace.placeMessage.persistence.UserReviewUuidRepository;
-import team7.inplace.token.persistence.RefreshTokenRepository;
 
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
-@EnableJpaRepositories(
-        basePackages = "team7.inplace",
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                classes = {RefreshTokenRepository.class, UserReviewUuidRepository.class}
-        )
-)
 @SpringBootApplication
 public class InplaceApplication {
 
