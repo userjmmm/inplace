78a115e4fa2c813e4c0029fca5eef4f9531ab911
wndlthsk <80496766+wndlthsk@users.noreply.github.com>
2025-01-21 12:49:24 +0900

    INPLACE-85 장소 세부 조회 시 influencerName, videoUrl response 변경 (#91)

--- backend/src/main/java/team7/inplace/place/presentation/dto/PlacesResponse.java
+++ backend/src/main/java/team7/inplace/place/presentation/dto/PlacesResponse.java
@@ -5,94 +5,111 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import team7.inplace.place.application.dto.PlaceQueryInfo;
 import team7.inplace.place.persistence.dto.PlaceQueryResult;
 import team7.inplace.review.persistence.dto.ReviewQueryResult;
+import team7.inplace.video.persistence.dto.VideoQueryResult.SimpleVideo;
 
 public class PlacesResponse {
+
     public record Simple(
-            Long placeId,
-            String placeName,
-            Address address,
-            String category,
-            String influencerName,
-            String menuImgUrl,
-            String longitude,
-            String latitude,
-            Boolean likes
+        Long placeId,
+        String placeName,
+        Address address,
+        String category,
+        String influencerName,
+        String menuImgUrl,
+        String longitude,
+        String latitude,
+        Boolean likes
     ) {
+
         public static List<Simple> from(List<PlaceQueryInfo.Simple> placeInfos) {
             return placeInfos.stream()
-                    .map(Simple::from)
-                    .toList();
+                .map(Simple::from)
+                .toList();
         }
 
         public static Simple from(PlaceQueryInfo.Simple placeInfo) {
             return new Simple(
-                    placeInfo.place().placeId(),
-                    placeInfo.place().placeName(),
-                    Address.from(
-                            placeInfo.place().address1(),
-                            placeInfo.place().address2(),
-                            placeInfo.place().address3()
-                    ),
-                    placeInfo.place().category(),
-                    placeInfo.video().get(0).influencerName(), //TODO : 인플루언서가 여러명일 경우 처리 필요
-                    placeInfo.place().menuImgUrl(),
-                    placeInfo.place().longitude().toString(),
-                    placeInfo.place().latitude().toString(),
-                    placeInfo.place().isLiked()
+                placeInfo.place().placeId(),
+                placeInfo.place().placeName(),
+                Address.from(
+                    placeInfo.place().address1(),
+                    placeInfo.place().address2(),
+                    placeInfo.place().address3()
+                ),
+                placeInfo.place().category(),
+                placeInfo.video().stream().map(SimpleVideo::influencerName)
+                    .filter(Objects::nonNull)
+                    .distinct()
+                    .collect(Collectors.joining(", ")),
+                placeInfo.place().menuImgUrl(),
+                placeInfo.place().longitude().toString(),
+                placeInfo.place().latitude().toString(),
+                placeInfo.place().isLiked()
             );
         }
     }
 
     public record Detail(
-            Long placeId,
-            String placeName,
-            Address address,
-            String category,
-            String influencerName,
-            String longitude,
-            String latitude,
-            Boolean likes,
-            JsonNode facilityInfo,
-            PlacesResponse.MenuInfos menuInfos,
-            PlacesResponse.OpenHour openHour,
-            PlacesResponse.PlaceLikes placeLikes,
-            String videoUrl
+        Long placeId,
+        String placeName,
+        Address address,
+        String category,
+        String influencerName,
+        String longitude,
+        String latitude,
+        Boolean likes,
+        JsonNode facilityInfo,
+        PlacesResponse.MenuInfos menuInfos,
+        PlacesResponse.OpenHour openHour,
+        PlacesResponse.PlaceLikes placeLikes,
+        List<String> videoUrl
     ) {
+
         public static Detail from(PlaceQueryInfo.Detail placeDetailInfo) {
             var placeBulk = placeDetailInfo.placeBulk();
             var videos = placeDetailInfo.videos();
             var placeReviewLikeRate = placeDetailInfo.reviewLikeRate();
 
             var menuInfos = PlacesResponse.MenuInfos.from(
-                    placeBulk.menuBordPhotos(),
-                    placeBulk.menus(),
-                    placeBulk.detailedPlace().menuUpdatedAt()
+                placeBulk.menuBordPhotos(),
+                placeBulk.menus(),
+                placeBulk.detailedPlace().menuUpdatedAt()
             );
             var openHour = PlacesResponse.OpenHour.from(placeBulk.openTimes(), placeBulk.offDays());
             var placeLikes = PlacesResponse.PlaceLikes.from(placeReviewLikeRate);
 
+            var influencerNames = videos.stream().map(SimpleVideo::influencerName)
+                .filter(Objects::nonNull)
+                .distinct()
+                .collect(Collectors.joining(", "));
+            var videoUrls = videos.stream()
+                .map(SimpleVideo::videoUrl)
+                .toList();
+
             return new Detail(
-                    placeBulk.detailedPlace().placeId(),
-                    placeBulk.detailedPlace().placeName(),
-                    Address.from(
-                            placeBulk.detailedPlace().address1(),
-                            placeBulk.detailedPlace().address2(),
-                            placeBulk.detailedPlace().address3()
-                    ),
-                    placeBulk.detailedPlace().category(),
-                    videos.get(0).influencerName(), //TODO : 인플루언서가 여러명일 경우 처리 필요
-                    placeBulk.detailedPlace().longitude().toString(),
-                    placeBulk.detailedPlace().latitude().toString(),
-                    placeBulk.detailedPlace().isLiked(),
-                    facilityTree(placeBulk.detailedPlace().facility()),
-                    menuInfos,
-                    openHour,
-                    placeLikes,
-                    videos.get(0).videoUUID()
+                placeBulk.detailedPlace().placeId(),
+                placeBulk.detailedPlace().placeName(),
+                Address.from(
+                    placeBulk.detailedPlace().address1(),
+                    placeBulk.detailedPlace().address2(),
+                    placeBulk.detailedPlace().address3()
+                ),
+                placeBulk.detailedPlace().category(),
+                influencerNames,
+                placeBulk.detailedPlace().longitude().toString(),
+                placeBulk.detailedPlace().latitude().toString(),
+                placeBulk.detailedPlace().isLiked(),
+                facilityTree(placeBulk.detailedPlace().facility()),
+                menuInfos,
+                openHour,
+                placeLikes,
+                videoUrls
             );
         }
 
@@ -109,100 +126,107 @@ public class PlacesResponse {
     }
 
     public record Address(
-            String address1,
-            String address2,
-            String address3
+        String address1,
+        String address2,
+        String address3
     ) {
+
         public static Address from(String address1, String address2, String address3) {
             return new Address(address1, address2, address3);
         }
     }
 
     public record MenuInfos(
-            List<String> menuImgUrls,
-            List<Menu> menuList,
-            String menuUpdatedAt
+        List<String> menuImgUrls,
+        List<Menu> menuList,
+        String menuUpdatedAt
     ) {
+
         public static MenuInfos from(
-                List<PlaceQueryResult.MenuBordPhoto> menuBoardPhotos,
-                List<PlaceQueryResult.Menu> menus,
-                LocalDateTime menuUpdatedAt
+            List<PlaceQueryResult.MenuBordPhoto> menuBoardPhotos,
+            List<PlaceQueryResult.Menu> menus,
+            LocalDateTime menuUpdatedAt
         ) {
             return new MenuInfos(
-                    menuBoardPhotos.stream().map(PlaceQueryResult.MenuBordPhoto::imgUrl).toList(),
-                    menus.stream().map(PlacesResponse.Menu::from).toList(),
-                    Optional.ofNullable(menuUpdatedAt)
-                            .map(dateTime -> dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-                            .orElse(null)
+                menuBoardPhotos.stream().map(PlaceQueryResult.MenuBordPhoto::imgUrl).toList(),
+                menus.stream().map(PlacesResponse.Menu::from).toList(),
+                Optional.ofNullable(menuUpdatedAt)
+                    .map(dateTime -> dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                    .orElse(null)
             );
         }
     }
 
     public record Menu(
-            String menuName,
-            String price,
-            String menuImgUrl,
-            Boolean recommended,
-            String description
+        String menuName,
+        String price,
+        String menuImgUrl,
+        Boolean recommended,
+        String description
     ) {
+
         public static Menu from(PlaceQueryResult.Menu menu) {
             return new Menu(
-                    menu.name(),
-                    menu.price(),
-                    menu.imgUrl(),
-                    menu.recommend(),
-                    menu.description()
+                menu.name(),
+                menu.price(),
+                menu.imgUrl(),
+                menu.recommend(),
+                menu.description()
             );
         }
     }
 
     public record OpenHour(
-            List<OpenTime> periodList,
-            List<OffDay> offdayList
+        List<OpenTime> periodList,
+        List<OffDay> offdayList
     ) {
+
         public static OpenHour from(
-                List<PlaceQueryResult.OpenTime> openTimes,
-                List<PlaceQueryResult.OffDay> closeTimes
+            List<PlaceQueryResult.OpenTime> openTimes,
+            List<PlaceQueryResult.OffDay> closeTimes
         ) {
             return new OpenHour(
-                    openTimes.stream().map(OpenTime::from).toList(),
-                    closeTimes.stream().map(OffDay::from).toList()
+                openTimes.stream().map(OpenTime::from).toList(),
+                closeTimes.stream().map(OffDay::from).toList()
             );
         }
     }
 
     public record OpenTime(
-            String timeName,
-            String timeSE,
-            String dayOfWeek
+        String timeName,
+        String timeSE,
+        String dayOfWeek
     ) {
+
         public static OpenTime from(PlaceQueryResult.OpenTime openTime) {
             return new OpenTime(
-                    openTime.timeName(),
-                    openTime.timeSE(),
-                    openTime.dayOfWeek()
+                openTime.timeName(),
+                openTime.timeSE(),
+                openTime.dayOfWeek()
             );
         }
     }
 
     public record OffDay(
-            String holidayName,
-            String weekAndDay,
-            String temporaryHolidays
+        String holidayName,
+        String weekAndDay,
+        String temporaryHolidays
     ) {
+
         public static OffDay from(PlaceQueryResult.OffDay offDay) {
             return new OffDay(
-                    offDay.holidayName(),
-                    offDay.weekAndDay(),
-                    offDay.temporaryHolidays()
+                offDay.holidayName(),
+                offDay.weekAndDay(),
+                offDay.temporaryHolidays()
             );
         }
     }
 
     public record PlaceLikes(
-            String like,
-            String dislike
+        String like,
+        String dislike
     ) {
+
         public static PlaceLikes from(ReviewQueryResult.LikeRate likeRate) {
             return new PlaceLikes(likeRate.likes().toString(), likeRate.dislikes().toString());
         }
