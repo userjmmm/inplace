47123b8f2f84fcc001c5d2fe32f9a2719cb3e190
sanghee0820 <lsh277604@gmail.com>
2025-02-04 14:02:03 +0900

    [INPLACE-121] 장소 카테고리 추가 (#126)

--- backend/src/main/java/team7/inplace/place/domain/Category.java
+++ backend/src/main/java/team7/inplace/place/domain/Category.java
@@ -11,6 +11,11 @@ public enum Category {
     JAPANESE("일식"),
     KOREAN("한식"),
     RESTAURANT("음식점"),
+    CHINESE("중식"),
+    CHICKEN("치킨"),
+    VIETNAMESE("베트남식"),
+    THAI("태국식"),
+    INDIAN("인도식"),
     NONE("없음");
 
     private final String name;
