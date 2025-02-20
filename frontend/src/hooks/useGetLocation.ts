9abd0b81c976d1058f6c453edd24b20dd132d414
userjmmm <141299582+userjmmm@users.noreply.github.com>
2025-02-21 00:38:59 +0900

    [INPLACE-164] useGetLocation 훅에서 enableHighAccuracy 제거 (#202)
    * [INPLACE-152] 세부페이지 credentials 추가 (#189)
    
    * [INPLACE-152] 마이페이지 관심장소 주소가 길 경우 말줄임표로 생략하도록 수정
    
    * [INPLACE-152] 방문할래요 모달 보내기 성공 시 메시지 수정
    
    * [INPLACE-152] 실제 accessToken 만료 시간에 맞추어 값 변경
    
    * [INPLACE-152] 모바일 필드값에 따라 메인 배너 필터링 로직 추가
    
    * [INPLACE-152] 지도 페이지에서 내위치 더 빨리 받아오도록 enableHighAccuracy 추가
    
    * [INPLACE-152] 메타 이미지 변경
    
    * [INPLACE-152] 백엔드 명세에 맞게 배너 필드값 수정
    
    * [INPLACE-152] s3에 올린 메타 이미지를 사용하도록 변경
    
    * [INPLACE-152] 실시간 마커 이미지 직접 가져오도록 수정
    
    * [INPLACE-152] 해상도 수정한 meta 이미지 적용
    
    * [INPLACE-152] 배너 누르면 해당 인플루언서 페이지로 이동하도록 수정
    
    * [INPLACE-152] influencerId 타입이 null인 경우 추가
    
    * [INPLACE-152] 리뷰탭 및 나에게 보내기 버튼 없애기
    
    * [INPLACE-152] 필요없는 코드 추가로 주석 처리
    
    * [INPLACE-152] 색상 앞에 따옴표 제거
    
    * [INPLACE-152] 세부페이지 credentials 추가
    
    ---------
    
    Co-authored-by: userjmmm <summer7j@naver.com>
    Co-authored-by: userjmmm <141299582+userjmmm@users.noreply.github.com>
    
    * [INPLACE-164] 모바일환경에서 실시간 위치를 자주 받아오는 빈도 줄이도록 enableHighAccuracy 제거
    
    * [INPLACE-164] queryClient에 retry 옵션을 1로 수정
    
    ---------
    
    Co-authored-by: sanghee0820 <sanghee0820.dev@gmail.com>
    Co-authored-by: hyoki <102338613+Hyoeunkh@users.noreply.github.com>

--- frontend/src/hooks/useGetLocation.ts
+++ frontend/src/hooks/useGetLocation.ts
@@ -2,22 +2,8 @@ import { useState, useEffect } from 'react';
 
 export default function useGetLocation() {
   const [location, setLocation] = useState<{ lat: number; lng: number } | null>(null);
-  const [isKakaoLoaded, setIsKakaoLoaded] = useState(false);
 
   useEffect(() => {
-    const checkKakaoLoaded = () => {
-      if (window.kakao && window.kakao.maps) {
-        setIsKakaoLoaded(true);
-      } else {
-        setTimeout(checkKakaoLoaded, 100);
-      }
-    };
-    checkKakaoLoaded();
-  }, []);
-
-  useEffect(() => {
-    if (!isKakaoLoaded) return undefined;
-
     if (navigator.geolocation) {
       const watchId = navigator.geolocation.watchPosition(
         (position) => {
@@ -30,14 +16,14 @@ export default function useGetLocation() {
         (error) => {
           console.error('Error fetching location', error);
         },
-        { enableHighAccuracy: true, maximumAge: 0, timeout: 5000 },
+        { maximumAge: 300000, timeout: 5000 },
       );
 
       return () => navigator.geolocation.clearWatch(watchId);
     }
     console.error('Geolocation is not supported by this browser.');
     return undefined;
-  }, [isKakaoLoaded]);
+  }, []);
 
   return location;
 }
