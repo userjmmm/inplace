63f26ead23bdd7b3f170498591a7cede8b08d368
sanghee0820 <sanghee0820.dev@gmail.com>
2025-02-22 18:53:50 +0900

    [INPLACE-163] 마진이 없도록 변경 (#211)

--- backend/src/main/java/team7/inplace/qrcode/application/QRCodeService.java
+++ backend/src/main/java/team7/inplace/qrcode/application/QRCodeService.java
@@ -1,12 +1,15 @@
 package team7.inplace.qrcode.application;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,12 +20,18 @@ public class QRCodeService {
 
         width = (width == null) ? 200 : width;
         height = (height == null) ? 200 : height;
+        Map<EncodeHintType, Object> hints = new HashMap<>();
 
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        hints.put(EncodeHintType.MARGIN, 0);
+
         BitMatrix bitMatrix;
 
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
         try {
-            bitMatrix = qrCodeWriter.encode(qrCodeData, BarcodeFormat.QR_CODE, width, height);
+
+            bitMatrix = multiFormatWriter.encode(qrCodeData, BarcodeFormat.QR_CODE, width, height,
+                hints);
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
