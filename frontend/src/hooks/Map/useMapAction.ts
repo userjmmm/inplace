cf5fd7ad42b3a89c10f855b1d83629d2db15cd5b
Hyoeunkh <lhe4528@knu.ac.kr>
2025-03-09 00:14:20 +0900

    [INPLACE-169] 마커 클릭 시 위치 이동 조절

--- frontend/src/hooks/Map/useMapAction.ts
+++ frontend/src/hooks/Map/useMapAction.ts
@@ -1,28 +1,22 @@
 import { useCallback } from 'react';
 
-export default function useMapActions({
-  mapRef,
-  isMobile,
-}: {
-  mapRef: React.MutableRefObject<kakao.maps.Map | null>;
-  isMobile: boolean;
-}) {
+export default function useMapActions(mapRef: React.MutableRefObject<kakao.maps.Map | null>) {
   const moveMapToMarker = useCallback(
     (latitude: number, longitude: number) => {
       if (!mapRef.current) return;
 
       const baseOffset = -0.007;
       const levelMultiplier = mapRef.current.getLevel() > 10 ? 2 : 1;
-      const offsetY = isMobile ? (baseOffset * levelMultiplier) / 5 : 0;
+      const offsetY = (baseOffset * levelMultiplier) / 3;
       const position = new kakao.maps.LatLng(latitude - offsetY, longitude);
 
-      if (mapRef.current.getLevel() > 10) {
-        mapRef.current.setLevel(9, { anchor: position, animate: true });
+      if (mapRef.current.getLevel() > 4) {
+        mapRef.current.setLevel(4, { anchor: position, animate: true });
       }
 
       setTimeout(() => mapRef.current?.panTo(position), 100);
     },
-    [isMobile, mapRef.current],
+    [mapRef.current],
   );
 
   const handleResetCenter = useCallback((userLocation: { lat: number; lng: number }) => {
