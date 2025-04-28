9ebe33ad3a8ec11cf9c402fc87eff097105d8d4c
userjmmm <summer7j@naver.com>
2025-04-28 12:10:50 +0900

    [INPLACE-216] 인플루언서 아이템 좋아요 아이콘에 그림자 추가

--- frontend/src/components/common/Items/InfluencerItem.tsx
+++ frontend/src/components/common/Items/InfluencerItem.tsx
@@ -68,7 +68,12 @@ export default function InfluencerItem({
             {isLike ? (
               <PiHeartFill color="#fe7373" size={32} data-testid="PiHeartFill" />
             ) : (
-              <PiHeartLight color="white" size={32} data-testid="PiHeartLight" />
+              <PiHeartLight
+                color="white"
+                size={32}
+                data-testid="PiHeartLight"
+                style={{ filter: 'drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.3))' }}
+              />
             )}
           </LikeIcon>
           <FallbackImage src={influencerImgUrl} alt={influencerName} />
