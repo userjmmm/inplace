22171db3abd1861d7b6f0fba46db59aff9a9cc83
Suhyeon7497 <nucc1186@gmail.com>
2025-04-29 21:21:51 +0900

    [INPLACE-218] user profile image 반환

--- backend/src/main/java/team7/inplace/user/application/dto/UserInfo.java
+++ backend/src/main/java/team7/inplace/user/application/dto/UserInfo.java
@@ -3,10 +3,11 @@ package team7.inplace.user.application.dto;
 import team7.inplace.user.domain.User;
 
 public record UserInfo(
-        String nickname
+        String nickname,
+        String profileImageUrl
 ) {
 
     public static UserInfo from(User user) {
-        return new UserInfo(user.getNickname());
+        return new UserInfo(user.getNickname(), user.getProfileImageUrl());
     }
 }
