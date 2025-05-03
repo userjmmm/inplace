4962b23b6e09fe9278ac831fb4985024841fff48
BaeJunH0 <bjh14757@naver.com>
2025-05-03 20:31:21 +0900

    [INPLACE-226] 영어로 된 에러 코드 한글로 변경

--- backend/src/main/java/team7/inplace/global/exception/code/AuthorizationErrorCode.java
+++ backend/src/main/java/team7/inplace/global/exception/code/AuthorizationErrorCode.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
-@Getter
 public enum AuthorizationErrorCode implements ErrorCode {
-    NOT_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "A000", "Authentication failed"),
-    TOKEN_IS_EMPTY(HttpStatus.BAD_REQUEST, "A001", "not include token"),
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "A002", "Invalid token"),
-    TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "A003", "token is expired");
+    NOT_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "A000", "인증이 실패했습니다."),
+    TOKEN_IS_EMPTY(HttpStatus.BAD_REQUEST, "A001", "요청에 토큰이 포함되어있지 않습니다."),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "A002", "유효하지 않은 토큰입니다."),
+    TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "A003", "만료된 토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorCode;
