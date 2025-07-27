1619d49134519c29bf4ae42034bebfba66c70c7a
BaeJunH0 <bjh14757@naver.com>
2025-07-27 21:09:16 +0900

    [INPLACE-258] 테스트 완료 조건 변경

--- backend/src/test/java/team7/inplace/alarm/FcmClientTest.java
+++ backend/src/test/java/team7/inplace/alarm/FcmClientTest.java
@@ -1,9 +1,12 @@
 package team7.inplace.alarm;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import team7.inplace.alarm.application.FcmClient;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class FcmClientTest {
@@ -13,7 +16,9 @@ public class FcmClientTest {
     @Test
     public void sendMessageByToken_realRequest() throws Exception {
         String testToken = "실제 FCM 토큰";
-        fcmClient.sendMessageByToken("테스트 제목", "테스트 본문", testToken);
-        // 예외 없으면 성공, 실패 시 FirebaseMessagingException 발생
+        
+        assertThrows(FirebaseMessagingException.class, () ->
+            fcmClient.sendMessageByToken("테스트 제목", "테스트 본문", testToken)
+        );
     }
 }
