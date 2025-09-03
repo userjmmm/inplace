c9a1e7d9b49ee8be8e4bdf9590c4701d98af54d8
hyunseo <wndlthsk@naver.com>
2025-09-03 20:22:09 +0900

    [INPLACE-285] @Param 추가

--- backend/inplace-infra/src/main/java/my/inplace/infra/user/jpa/UserJpaRepository.java
+++ backend/inplace-infra/src/main/java/my/inplace/infra/user/jpa/UserJpaRepository.java
@@ -1,16 +1,17 @@
 package my.inplace.infra.user.jpa;
 
 import java.util.Optional;
+import my.inplace.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import my.inplace.domain.user.User;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.username = :username AND u.deleteAt IS NULL")
-    Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(@Param("username") String username);
 
     @Query("SELECT u.id FROM User u WHERE u.nickname = :nickname")
     Optional<Long> findIdByNickname(String nickname);
