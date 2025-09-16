6608c06009f93fa28d84585561292bc0141f1339
Suhyeon7497 <nucc1186@gmail.com>
2025-09-16 21:01:10 +0900

    [INPLACE-294] influencer getfluencerDetail test

---
+++ backend/inplace-infra/src/test/java/my/inplace/infra/config/TestQueryDslConfig.java
@@ -0,0 +1,17 @@
+package my.inplace.infra.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+
+public class TestQueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
---
+++ backend/inplace-infra/src/test/java/my/inplace/infra/influencer/InfluencerReadQueryDslRepositoryTest.java
@@ -0,0 +1,56 @@
+package my.inplace.infra.influencer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import my.inplace.domain.influencer.query.InfluencerQueryResult;
+import my.inplace.infra.config.TestQueryDslConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest(
+    includeFilters = @Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        value = InfluencerReadQueryDslRepository.class
+    )
+)
+@Sql(scripts = "/sql/test-influencer.sql")
+@Import(TestQueryDslConfig.class)
+@EntityScan("my.inplace.domain")
+class InfluencerReadQueryDslRepositoryTest {
+
+    @Autowired
+    InfluencerReadQueryDslRepository influencerRepository;
+
+
+    @Test
+    void getInfluencerDetail() {
+        // given
+        Long influencerId = 2L;
+        Long userId = 1L;
+        InfluencerQueryResult.Detail expected = new InfluencerQueryResult.Detail(
+            2L, "인플루언서2", "img2", "직업2", true, 4L, 0L
+        );
+
+        // when
+        Optional<InfluencerQueryResult.Detail> actual = influencerRepository.getInfluencerDetail(influencerId, userId);
+
+        //then
+        assertThat(actual.isPresent()).isTrue();
+        assertThat(actual.get()).isEqualTo(expected);
+    }
+
+    @Test
+    void getInfluencerNamesByPlaceId() {
+    }
+
+    @Test
+    void getInfluencerSortedByLikes() {
+    }
+}
